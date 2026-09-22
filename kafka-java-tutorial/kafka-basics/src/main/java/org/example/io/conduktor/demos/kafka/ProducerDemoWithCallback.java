922484f309cb871b6cde241855e848ff9fb8d7d4
BogdanSicu <b0gdan.munt3@gmail.com>
2026-09-22 13:19:28 +0300

    Sending data in batches

---
+++ kafka-java-tutorial/kafka-basics/src/main/java/org/example/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,78 @@
+package org.example.io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("-----------------I am a Kafka Producer----------------------");
+
+//region create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        properties.setProperty("batch.size", "400");
+//endregion
+
+//region create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for(int j=0; j<10; j++) { // kafka saves records in batches if sent multiple at a time
+
+            for (int i = 0; i < 30; i++) {
+
+                //create Producer Record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello World " + i);
+
+                //region send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        // executes every time a record is successfully sent or an exception is thrown
+                        if (exception == null) {
+                            //the record was successfully sent
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offset: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp() + "\n");
+                        } else {
+                            log.error("Error while producing", exception);
+                        }
+                    }
+                });
+                //endregion
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+//endregion
+
+
+//region flush and close the producer
+        // tell the producer to send all data and block until done -- synchronous operation
+        producer.flush();
+
+        producer.close(); // this also calls producer.flush() before closing
+//endregion
+    }
+}
